c9b7bbfb51eaf36a134a62fa167b7553d5489392
Chungon <cotse900@gmail.com>
2023-06-26 16:39:46 -0400

    interface

---
+++ ws08/Task1/DoubleArrayFunctions.java
@@ -0,0 +1,26 @@
+/**********************************************
+ Workshop 8
+ Course: JAC444 - Semester 4
+ Last Name: Tse
+ First Name: Chungon
+ ID: 154928188
+ Section: NAA
+ This assignment represents my own work in accordance with Seneca Academic Policy.
+ CHUNGON
+ Date: 28 Mar 2023
+ **********************************************/
+package ws08_task1;
+/**
+ * interface
+ */
+@FunctionalInterface
+public interface DoubleArrayFunctions {
+    /**
+     * apply double
+     * @param array     array
+     * @return          array
+     */
+    double applyDouble(double[] array);
+}
+
+
